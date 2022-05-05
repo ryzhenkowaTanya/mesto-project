e65127a5b71d13d39d32dac4731290c68ed0cf82
Tatyana Ryzhenkowa <ryzhenkowa.tatyana@yandex.ru>
2022-05-05 19:18:38 +0300

    fix

--- src/components/card.js
+++ src/components/card.js
@@ -10,6 +10,7 @@ export const nameInputCard = document.querySelector('.popup__input_type_name-car
 export const linkInputCard = document.querySelector('.popup__input_type_link');
 const cardList = document.querySelector('.cards__list');
 
+
 export const initialCards = [
     {
         name: 'Pulau Ubin',
